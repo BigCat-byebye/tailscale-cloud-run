49778467e79c5c30b5b0b452a449e70e4f978435
拜拜是大猫 <32433624+BigCat-byebye@users.noreply.github.com>
2025-08-06 15:06:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /app/tailscaled --tun=userspace-networking --socks5-server=localhost:1055 &
-until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=cloudrun-app
+until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=cloudrun-app --advertise-connector --advertise-tags=tag:cloud-run-connector
 do
     sleep 0.1
 done
