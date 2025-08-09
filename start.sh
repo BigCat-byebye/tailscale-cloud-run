9e9aa895a3467c03a9a1e4fea2361698afcbe33f
拜拜是大猫 <32433624+BigCat-byebye@users.noreply.github.com>
2025-08-09 14:54:54 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-/app/tailscaled --tun=userspace-networking --socks5-server=0.0.0.0:1055 &
+/app/tailscaled --tun=userspace-networking &
 until /app/tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=cloudrun --advertise-exit-node --advertise-tags=tag:cloudrun
 do
     sleep 0.1
 done
 echo Tailscale started
-ALL_PROXY=socks5://0.0.0.0:1055/ node --inspect index.js
+node --inspect index.js
